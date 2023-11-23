32f8c5cbdab40e8eb42b59d07ef4b5dfa41bb858
JerryQ <1397200108@qq.com>
2023-11-23 14:26:58 +0800

    将被装饰的类的文档复制到单例模式类中

--- server/src/utils/singleton.py
+++ server/src/utils/singleton.py
@@ -1,16 +1,27 @@
+import functools
 from typing import Generic, TypeVar
 
 _T_co = TypeVar("_T_co", bound=object, covariant=True)
 
 
 class Singleton(Generic[_T_co]):
-    """Singleton class, use it as a decorator"""
+    """
+    Singleton class, use it as a decorator
+
+    Example:
+    ```
+    @Singleton
+    class A:
+        pass
+    ```
+    """
 
     __slots__ = ("_cls", "_instance")
 
     def __init__(self, cls: type[_T_co]):
         self._cls = cls
         self._instance: object | None = None
+        functools.wraps(self.__call__)(cls.__init__)
 
     def __call__(self, *args, **kwargs) -> _T_co:
         if self._instance is None:
@@ -19,13 +30,21 @@ class Singleton(Generic[_T_co]):
 
 
 def singleton(cls: type[_T_co]) -> Singleton[_T_co]:
-    """Singleton decorator, use it as a decorator"""
+    """Singleton decorator, alias of class Singleton"""
 
     return Singleton(cls)
 
 
 class SingletonMeta(type):
-    """Singleton metaclass, use it as a metaclass"""
+    """
+    Singleton metaclass, use it as a metaclass
+
+    Example:
+    ```
+    class A(metaclass=SingletonMeta):
+        pass
+    ```
+    """
 
     __slots__ = ()
 
