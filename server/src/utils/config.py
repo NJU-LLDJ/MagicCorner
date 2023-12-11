0674a1a7b1a8d21d55fd5c63352bf39873317870
JerryQ <1397200108@qq.com>
2023-12-11 17:26:08 +0800

    FIX mypy error `error: "type[BaseModel]" has no attribute "model_validate_json"`

--- server/src/utils/config.py
+++ server/src/utils/config.py
@@ -1,8 +1,30 @@
+import json
+from _typeshed import OpenTextMode
+from os import PathLike
+from types import ModuleType
+from typing import IO, Protocol, runtime_checkable, TypeVar, Any
+
 from pydantic import BaseModel
+
+_L_co = TypeVar("_L_co", bound=dict, covariant=True)
+
+
+@runtime_checkable
+class SupportsLoad(Protocol[_L_co]):
+    @staticmethod
+    def load(buffer: IO) -> _L_co:
+        raise NotImplementedError
 
 
 class BaseConfig(BaseModel):
     @classmethod
-    def from_file(cls, path: str):
-        with open(path) as file:
-            return cls.model_validate_json(file.read())
+    def load(
+        cls,
+        path: str | bytes | PathLike[str] | PathLike[bytes],
+        mode: OpenTextMode = "r",
+        encoding: str | None = None,
+        serialize_module: SupportsLoad[dict[str, Any]] | ModuleType = json,
+    ) -> "BaseConfig":
+        with open(path, mode, encoding=encoding) as file:
+            d = serialize_module.load(file)
+        return cls(**d)
