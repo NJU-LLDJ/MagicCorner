57d2c74769491c86155f4c6eec98291c2a02325e
JerryQ <1397200108@qq.com>
2023-12-12 16:51:18 +0800

    refactor: 修改`Singleton`的实现使其重用`SingletonMeta`
    原先`Singleton`用装饰器实现，`singleton`函数仅仅是`Singleton`的alias。
    在此次committ后，`Singleton`直接使用`SingletonMeta`创建类，导致`singleton`函数被迫使用一个辅助类以实现单例模式。
    这个变化导致了一个新的问题：IDE不能正常对`@singleton`装饰的类进行代码提示。
    不过，直接继承`Singleton`或设置元类为`SingletonMeta`都没有出现这种情况，即使使用`@singleton`也不会影响已有的代码，所以这个问题并不是很严重。
    这个问题优先级不高，留待以后观察。
    
    Closes #4

--- server/src/utils/singleton.py
+++ server/src/utils/singleton.py
@@ -1,42 +1,5 @@
-import functools
-from typing import Generic, TypeVar
-
-_T_co = TypeVar("_T_co", bound=object, covariant=True)
-
-
-class Singleton(Generic[_T_co]):
-    """
-    Singleton class, use it as a decorator
-
-    Example:
-    ```
-    @Singleton
-    class A:
-        pass
-    ```
-    """
-
-    __slots__ = ("_cls", "_instance")
-
-    def __init__(self, cls: type[_T_co]):
-        self._cls = cls
-        self._instance: object | None = None
-        functools.wraps(self.__call__)(cls.__init__)
-
-    def __call__(self, *args, **kwargs) -> _T_co:
-        if self._instance is None:
-            self._instance = self._cls(*args, **kwargs)
-        return self._instance
-
-    @property
-    def instance(self) -> _T_co:
-        return self._instance
-
-
-def singleton(cls: type[_T_co]) -> Singleton[_T_co]:
-    """Singleton decorator, alias of class Singleton"""
-
-    return Singleton(cls)
+import warnings
+from typing import Optional
 
 
 class SingletonMeta(type):
@@ -44,23 +7,73 @@ class SingletonMeta(type):
     Singleton metaclass, use it as a metaclass
 
     Example:
-    ```
-    class A(metaclass=SingletonMeta):
-        pass
-    ```
+        >>> class A(metaclass=SingletonMeta):
+        ...     def __init__(self, n: int):
+        ...         self.n = n
+
+        >>> a = A(1)
+        >>> b = A(2)
+        >>> a is b
+        True
+        >>> a.n == b.n == 1
+        True
     """
 
-    _instance: _T_co | None = None
+    _instance: Optional[object] = None
 
-    __slots__ = ()
-
-    def __call__(cls: type[_T_co], *args, **kwargs) -> _T_co:
+    def __call__(cls, *args, **kwargs):
         if cls._instance is None:
             cls._instance = super().__call__(*args, **kwargs)
         return cls._instance
 
     @property
-    def instance(cls) -> _T_co | None:
+    def instance(cls):
         if cls._instance is None:
-            raise RuntimeWarning("Singleton instance is not initialized")
+            warnings.warn(f"Singleton Class {cls.__qualname__} is not initialized")
         return cls._instance
+
+
+class Singleton(metaclass=SingletonMeta):
+    """
+    Singleton class, inherit it to make a singleton class
+
+    Example:
+        >>> class A(Singleton):
+        ...     def __init__(self, n: int):
+        ...         self.n = n
+
+        >>> a = A(1)
+        >>> b = A(2)
+        >>> a is b
+        True
+        >>> a.n == b.n == 1
+        True
+    """
+
+
+def singleton(cls: type):
+    """
+    Singleton decorator, use it as a decorator
+
+    Issue:
+        This decorator makes it hard for IDE to recognize the type of the decorated class.
+        It is recommended to use the Singleton class or SingletonMeta metaclass instead.
+
+    Example:
+        >>> @singleton
+        ... class A:
+        ...     def __init__(self, n: int):
+        ...         self.n = n
+
+        >>> a = A(1)
+        >>> b = A(2)
+        >>> a is b
+        True
+        >>> a.n == b.n == 1
+        True
+    """
+
+    class SingletonWrapper(cls, metaclass=SingletonMeta):
+        pass
+
+    return SingletonWrapper
