dbf534150f1e36d9ebe845210dbcc9a9570c7417
JerryQ <1397200108@qq.com>
2023-12-12 17:49:07 +0800

    refactor: 减少代码冗余，修改了一些类型注释
    减少代码冗余：
    - 我发现不需要`SyncExecutor`作为抽象基类，实际上把不同的池传给它作为参数即可，因此删除了`ThreadSyncExecutor`和`ProcessSyncExecutor`。
    修改类型注释：
    - 根据标准库中`collections.abc.Coroutine`的定义，添加了一些协变或逆变的约束。
    - 根据mypy提示，修改了一些签名。
    
    BREAKING CHANGE: 删除了`ThreadSyncExecutor`和`ProcessSyncExecutor`。

--- server/src/utils/async_functools.py
+++ server/src/utils/async_functools.py
@@ -1,83 +1,62 @@
-import functools
-import pickle
-from abc import ABC, abstractmethod
 from collections.abc import Callable, Coroutine
-from concurrent.futures import Future, ProcessPoolExecutor, ThreadPoolExecutor
-from typing import TypeVar, ParamSpec
+from concurrent.futures import Executor, ProcessPoolExecutor, ThreadPoolExecutor
+from functools import wraps
 from multiprocessing import reduction
+from typing import TypeVar, ParamSpec
 
-import dill
-
-from utils.singleton import singleton
+import dill  # type: ignore
 
 # 标准库pickle模块不支持对某些函对象的序列化，使用dill模块替代
-reduction.ForkingPickler.dumps = dill.dumps
-reduction.ForkingPickler.loads = dill.loads
+reduction.ForkingPickler.dumps = dill.dumps  # type: ignore
+reduction.ForkingPickler.loads = dill.loads  # type: ignore
 
-_YT = TypeVar("_YT")
-_ST = TypeVar("_ST")
-_T = TypeVar("_T")
 _P = ParamSpec("_P")
+_RT_co = TypeVar("_RT_co")
+_ST_contra = TypeVar("_ST_contra")
+_YT_co = TypeVar("_YT_co")
 
 
-class SyncExecutor(ABC):
+class SyncExecutor:
     """同步执行器抽象基类，用于同步执行异步函数，子类需实现run方法"""
 
-    __slots__ = ()
-    _pickle_module = pickle
+    __slots__ = ("_pool",)
 
-    @classmethod
-    def set_pickle_module(cls, module):
-        """设置pickle模块，用于序列化函数对象"""
-        if not hasattr(module, "loads") or not hasattr(module, "dumps"):
-            raise TypeError("module must have loads and dumps methods")
-        cls._pickle_module = module
+    def __init__(self, pool: Executor):
+        """
+        初始化同步执行器
+        Args:
+            pool: 线程池或进程池
+        """
+        self._pool = pool
 
     @staticmethod
     def _async_run(
-        func: Callable[_P, Coroutine[_YT, _ST, _T]] | bytes,
+        func: Callable[_P, Coroutine[_YT_co, _ST_contra, _RT_co]],
+        /,
         *args: _P.args,
-        **kwargs: _P.kwargs
-    ):
-        """作为多线程或多进程的目标函数，在新事件循环中运行异步函数，func可以是函数对象或序列化后的bytes"""
-        import asyncio  # 避免在子进程中报错 NameError: name 'asyncio' is not defined
+        **kwargs: _P.kwargs,
+    ) -> _RT_co:
+        """
+        作为多线程或多进程的目标函数，在新事件循环中运行异步函数，
+        Args:
+            func: 异步函数
+            *args: func的位置参数
+            **kwargs: func的关键字参数
+
+        Returns:
+            func的返回值
+        """
+        import asyncio  # 避免报错 NameError: name 'asyncio' is not defined
 
         loop = asyncio.new_event_loop()
-        if isinstance(func, bytes):
-            func = SyncExecutor._pickle_module.loads(func)
         return loop.run_until_complete(func(*args, **kwargs))
 
-    @abstractmethod
     def run(
         self,
-        func: Callable[_P, Coroutine[_YT, _ST, _T]],
+        func: Callable[_P, Coroutine[_YT_co, _ST_contra, _RT_co]],
         *args: _P.args,
-        **kwargs: _P.kwargs
-    ) -> _T:
-        """子类需实现本方法，用_async_run运行异步函数，阻塞直至返回结果"""
-
-
-@singleton
-class ProcessSyncExecutor(SyncExecutor):
-    """同步执行器，异步任务运行于进程池内，单例"""
-
-    __slots__ = ("_pool",)
-    _pickle_module = dill
-
-    def __init__(self, processes: int | None = None):
-        """
-        初始化进程池
-        Args:
-            processes: 进程数，默认为os.cpu_count() or 1
-        """
-        self._pool = ProcessPoolExecutor(processes)
-
-    def run(
-        self,
-        func: Callable[_P, Coroutine[_YT, _ST, _T]],
-        *args: _P.args,
-        **kwargs: _P.kwargs
-    ) -> _T:
+        **kwargs: _P.kwargs,
+    ) -> _RT_co:
         """
         运行异步函数并同步返回结果，阻塞直至返回结果
         Args:
@@ -88,80 +67,17 @@ class ProcessSyncExecutor(SyncExecutor):
         Returns:
             func的返回值
         """
-        fut: Future[_T] = self._pool.submit(self._async_run, *(func, *args), **kwargs)
-        return fut.result()
+        return self._pool.submit(self._async_run, func, *args, **kwargs).result()
 
 
-@singleton
-class ThreadSyncExecutor(SyncExecutor):
-    """同步执行器，异步任务运行于线程池内，单例"""
-
-    __slots__ = ("_pool",)
-
-    def __init__(self, max_workers: int | None = None):
-        """
-        初始化线程池
-        Args:
-            max_workers: 线程数，默认为min(32, (os.cpu_count() or 1) + 4)
-        """
-        self._pool = ThreadPoolExecutor(max_workers)
-
-    def run(
-        self,
-        func: Callable[_P, Coroutine[_YT, _ST, _T]],
-        *args: _P.args,
-        **kwargs: _P.kwargs
-    ) -> _T:
-        """
-        运行异步函数并同步返回结果，阻塞直至返回结果
-        Args:
-            func: 异步函数
-            *args: func的位置参数
-            **kwargs: func的关键字参数
-
-        Returns:
-            func的返回值
-        """
-        fut: Future[_T] = self._pool.submit(self._async_run, *(func, *args), **kwargs)
-        return fut.result()
-
-
-def run_thread_sync(
-    func: Callable[_P, Coroutine[_YT, _ST, _T]], *args: _P.args, **kwargs: _P.kwargs
-) -> _T:
-    """
-    使用线程池运行异步函数并同步返回结果，阻塞直至返回结果
-    Args:
-        func: 异步函数
-        *args: func的位置参数
-        **kwargs: func的关键字参数
-
-    Returns:
-        func的返回值
-    """
-    return ThreadSyncExecutor().run(func, *args, **kwargs)
-
-
-def thread_sync(func: Callable[_P, Coroutine[_YT, _ST, _T]]) -> Callable[_P, _T]:
-    """
-    被装饰的异步函数将在新线程中运行，阻塞直至返回结果，可被视为同步函数
-    Args:
-        func: 异步函数
-
-    Returns:
-        同步化的func
-    """
-
-    @functools.wraps(func)
-    def wrapper(*args: _P.args, **kwargs: _P.kwargs) -> _T:
-        return run_thread_sync(func, *args, **kwargs)
-
-    return wrapper
+process_sync_executor = SyncExecutor(ProcessPoolExecutor())
 
 
 def run_process_sync(
-    func: Callable[_P, Coroutine[_YT, _ST, _T]], *args: _P.args, **kwargs: _P.kwargs
-) -> _T:
+    func: Callable[_P, Coroutine[_YT_co, _ST_contra, _RT_co]],
+    *args: _P.args,
+    **kwargs: _P.kwargs,
+) -> _RT_co:
     """
     使用进程池运行异步函数并同步返回结果，阻塞直至返回结果
     Args:
@@ -172,10 +88,12 @@ def run_process_sync(
     Returns:
         func的返回值
     """
-    return ProcessSyncExecutor().run(func, *args, **kwargs)
+    return process_sync_executor.run(func, *args, **kwargs)
 
 
-def process_sync(func: Callable[_P, Coroutine[_YT, _ST, _T]]) -> Callable[_P, _T]:
+def process_sync(
+    func: Callable[_P, Coroutine[_YT_co, _ST_contra, _RT_co]]
+) -> Callable[_P, _RT_co]:
     """
     被装饰的异步函数将在新进程中运行，阻塞直至返回结果，可被视为同步函数
     Args:
@@ -185,9 +103,49 @@ def process_sync(func: Callable[_P, Coroutine[_YT, _ST, _T]]) -> Callable[_P, _T
         同步化的func
     """
 
-    @functools.wraps(func)
-    def wrapper(*args: _P.args, **kwargs: _P.kwargs) -> _T:
+    @wraps(func)
+    def wrapper(*args: _P.args, **kwargs: _P.kwargs) -> _RT_co:
         return run_process_sync(func, *args, **kwargs)
+
+    return wrapper
+
+
+thread_sync_executor = SyncExecutor(ThreadPoolExecutor())
+
+
+def run_thread_sync(
+    func: Callable[_P, Coroutine[_YT_co, _ST_contra, _RT_co]],
+    *args: _P.args,
+    **kwargs: _P.kwargs,
+) -> _RT_co:
+    """
+    使用线程池运行异步函数并同步返回结果，阻塞直至返回结果
+    Args:
+        func: 异步函数
+        *args: func的位置参数
+        **kwargs: func的关键字参数
+
+    Returns:
+        func的返回值
+    """
+    return thread_sync_executor.run(func, *args, **kwargs)
+
+
+def thread_sync(
+    func: Callable[_P, Coroutine[_YT_co, _ST_contra, _RT_co]]
+) -> Callable[_P, _RT_co]:
+    """
+    被装饰的异步函数将在新线程中运行，阻塞直至返回结果，可被视为同步函数
+    Args:
+        func: 异步函数
+
+    Returns:
+        同步化的func
+    """
+
+    @wraps(func)
+    def wrapper(*args: _P.args, **kwargs: _P.kwargs) -> _RT_co:
+        return run_thread_sync(func, *args, **kwargs)
 
     return wrapper
 
